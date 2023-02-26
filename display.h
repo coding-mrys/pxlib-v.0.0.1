c7ec65b2838f3568024c7f785e5af5ef9bb7e978
mrys <80972359+coding-mrys@users.noreply.github.com>
2023-02-26 15:34:54 +0100

    Delete display.h

--- display.h
+++
@@ -1,9 +0,0 @@
-using namespace std;
-
-class display {
-public:
-	vector<float>xvalues = { 0,0,0,0 };
-	vector<float>yvalues = { 0,0,0,0 };
-	vector<float>zvalues = { 0,0,0,0 };
-};
-display screen;
